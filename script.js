799b2d77cf5c89bd1964183ea0c1c590612b507c
Maria Jenkins <macjenkin@gmail.com>
2023-03-16 20:35:55 -0500

    add divs to index.html

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 const h2 = document.createElement("h2");
-h2.textContent = "Also known as The Black Knights' Tango, the Mexican Defense, Two Knights' Tango, and Kevitz-Trajkovic Defense";
+h2.textContent = "The Knights' Tango is an opening by black in response to the Queen's Gambit";
 
 document.querySelector("body").appendChild(h2);
