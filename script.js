0bbf74e520318f9637fcfbbb6639511dd9060c89
Maria Jenkins <macjenkin@gmail.com>
2023-03-17 00:08:42 -0500

    improve board, center text

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
-var input = document.getElementById('board');
-input.addEventListener('click', moveForward)
+var image = document.getElementById('board');
+image.addEventListener('click', moveForward)
 
 function moveForward() {
-    if (input.src="Media/startingposition.png") {
+    var input = document.getElementById('board');
+    if (input.src.match("Media/startingposition.png")) {
+        input.src="Media/move1a.png";
+    } else if (input.src.match("Media/move1a.png")) {
+        input.src="Media/move1b.png";
+    } else if (input.src.match("Media/move1b.png")) {
+        input.src="Media/move2a.png";
+    } else if (input.src.match("Media/move2a.png")) {
         input.src="Media/move2b.png";
+    } else if (input.src.match("Media/move2b.png")) {
+        input.src="Media/startingposition.png";
     }
 }
